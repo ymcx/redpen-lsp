0a994dde8d1df2a2a660c1037430142bb56dc6cf
ymcx <89810988+ymcx@users.noreply.github.com>
2025-05-08 00:03:55 +0300

    add error highlighting

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
+import re
+import hunspell
 from pygls.server import LanguageServer
 from lsprotocol.types import (
     Diagnostic,
     DiagnosticSeverity,
+    DidChangeTextDocumentParams,
     Position,
     Range,
     TEXT_DOCUMENT_DID_CHANGE,
@@ -10,18 +13,42 @@ from lsprotocol.types import (
 
 
 class LSP(LanguageServer):
+    def get_diagnostics(self, document):
+        diagnostics = []
+        line = 0
+        line_start = 0
+
+        for word_boundary in re.finditer(r"\S+", document):
+            start, end = word_boundary.start(), word_boundary.end()
+            word = document[start:end]
+
+            word_lines = document.count("\n", line_start, start)
+            if word_lines > 0:
+                line += word_lines
+                line_start = document.rfind("\n", 0, start) + 1
+
+            if not self.hunspell.spell(word):
+                range = Range(
+                    Position(line, start - line_start),
+                    Position(line, end - line_start),
+                )
+                diagnostic = Diagnostic(range, "", DiagnosticSeverity.Error)
+                diagnostics.append(diagnostic)
+
+        return diagnostics
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.hunspell = hunspell.HunSpell(
+            "/usr/share/hunspell/en_US.dic", "/usr/share/hunspell/en_US.aff"
+        )
 
         @self.feature(TEXT_DOCUMENT_DID_CHANGE)
         @self.feature(TEXT_DOCUMENT_DID_OPEN)
-        def update(params):
-            diagnostic = Diagnostic(
-                Range(Position(0, 0), Position(0, 5)),
-                "TEST",
-                DiagnosticSeverity.Error,
-            )
-            self.publish_diagnostics(params.text_document.uri, [diagnostic])
+        def _(params: DidChangeTextDocumentParams):
+            document = self.workspace.get_document(params.text_document.uri).source
+            diagnostics = self.get_diagnostics(document)
+            self.publish_diagnostics(params.text_document.uri, diagnostics)
 
 
 def main():
